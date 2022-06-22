870d4afbb1d99803bc3104ce62604f1e0173c4f3
Nicklzy <tydx11@hotmail.com>
2022-06-22 20:53:10 +0800

    命名修正

--- src/main/java/hello/configuration/JavaConfiguration.java
+++ src/main/java/hello/configuration/JavaConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JavaConfigRation {
+public class JavaConfiguration {
     @Bean
     public UserService userService(UserMapper userMapper) {
         return new UserService(userMapper);
